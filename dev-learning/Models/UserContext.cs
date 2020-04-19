4ecfdd950dc7a8ea50e1691f0e40cacd2331e3a9
Edas Lakavicius <elakavicius@gmail.com>
2020-04-19 21:08:06 +0300

    Add user context fro entity framework

---
+++ dev-learning/Models/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace dev_learning.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["dbConfig"].ConnectionString;
+
+            _ = optionsBuilder.UseMySQL(connectionString);
+        }
+    }
+}
