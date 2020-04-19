c8b1b2d29e997bc97d4fb39bd4ea0fecc9d173ab
NeriusZar <neriuszarnauskas@gmail.com>
2020-04-19 17:14:04 +0300

    Remove example files

--- dev-learning/Startup.cs
+++ dev-learning/Startup.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using dev_learning.Models;
 
 namespace dev_learning
 {
@@ -27,8 +18,6 @@ namespace dev_learning
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TestModelContext>(opt =>
-            opt.UseInMemoryDatabase("testDB"));
             services.AddControllers();
         }
 
