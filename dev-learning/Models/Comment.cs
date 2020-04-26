c3dc2a6d2eb03520189bb3d6e9c0c8c931cb4a32
hjuknonis <h.juknonis@gmail.com>
2020-04-26 12:34:20 +0300

    added comment model

---
+++ dev-learning/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dev_learning.Models
+{
+   
+    public class Comment
+    {
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int SubjectId { get; set; }
+        public string Text { get; set; }
+        public DateTime Datetime { get; set; }
+
+    }
+}
