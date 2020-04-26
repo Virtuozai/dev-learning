17b6bc6d6f45bfac68f2a2d0a580a29dfb62a68e
NeriusZar <neriuszarnauskas@gmail.com>
2020-04-26 17:59:59 +0300

    Rename users database

--- dev-learning/Controllers/UsersController.cs
+++ dev-learning/Controllers/UsersController.cs
@@ -22,14 +22,14 @@ namespace dev_learning.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.User.ToListAsync();
         }
 
         // GET: api/Users/5
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUser(int id)
         {
-            var user = await _context.Users.FindAsync(id);
+            var user = await _context.User.FindAsync(id);
 
             if (user == null)
             {
@@ -72,7 +72,7 @@ namespace dev_learning.Controllers
         [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
-            _context.Users.Add(user);
+            _context.User.Add(user);
             await _context.SaveChangesAsync();
             return CreatedAtAction("GetUser", new { id = user.id }, user);
         }
@@ -81,13 +81,13 @@ namespace dev_learning.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult<User>> DeleteUser(int id)
         {
-            var user = await _context.Users.FindAsync(id);
+            var user = await _context.User.FindAsync(id);
             if (user == null)
             {
                 return NotFound();
             }
 
-            _context.Users.Remove(user);
+            _context.User.Remove(user);
             await _context.SaveChangesAsync();
 
             return user;
@@ -97,7 +97,7 @@ namespace dev_learning.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(AuthRequest authRequest)
         {
-            var users = await _context.Users.ToListAsync();
+            var users = await _context.User.ToListAsync();
             var isUserValid = users.Exists(user => user.email == authRequest.email && user.password == authRequest.password);
 
             if (isUserValid)
@@ -110,7 +110,7 @@ namespace dev_learning.Controllers
 
         private bool UserExists(int id)
         {
-            return _context.Users.Any(e => e.id == id);
+            return _context.User.Any(e => e.id == id);
         }
     }
 }
