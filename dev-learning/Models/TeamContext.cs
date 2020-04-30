829577f82a274d867679f4d5b2f5ec325479304b
GytisGrigonis <grigonis0@gmail.com>
2020-04-30 17:43:17 +0300

    teams logic added

---
+++ dev-learning/Models/TeamContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dev_learning.Models
+{
+    class TeamContext : DbContext
+    {
+        public TeamContext(DbContextOptions<TeamContext> options)
+            :   base(options)
+        {
+        }
+
+        public DbSet<Team> Teams { get; set; }
+    }
+}
