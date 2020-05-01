391022d17e370d6d134bd86682f3751f9fc32a62
hjuknonis <h.juknonis@gmail.com>
2020-05-01 14:44:41 +0300

    startup fix

--- dev-learning/Startup.cs
+++ dev-learning/Startup.cs
@@ -26,16 +26,7 @@ namespace dev_learning
 
             services.AddControllers().AddNewtonsoftJson(options =>
             options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-           
-            services.AddMvc();
-            services
-                .AddControllers()
-                .AddNewtonsoftJson(options =>
-                  {
-                     options.SerializerSettings.ReferenceLoopHandling =
-                     Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                });
-
+          
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
