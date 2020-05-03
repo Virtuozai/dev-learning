7706ff56470fd4aa9ab2e980a442a1b0373b2028
hjuknonis <h.juknonis@gmail.com>
2020-05-03 14:27:02 +0300

    changed userSubject model

--- dev-learning/Models/UserSubject.cs
+++ dev-learning/Models/UserSubject.cs
@@ -1,16 +1,18 @@
 ﻿
 
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace dev_learning.Models
 {
     public class UserSubject
     {
+        public int Id { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
         public int SubjectId { get; set; }
         public Subject Subject { get; set; }
-
-        public int DaysLearned { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
     }
 }
