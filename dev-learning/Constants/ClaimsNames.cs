cd71c175df98b0956410481d37eabaf6171766ab
NeriusZar <neriuszarnauskas@gmail.com>
2020-05-15 21:49:05 +0300

    Delete jwt token files

--- dev-learning/Constants/ClaimsNames.cs
+++ dev-learning/Constants/ClaimsNames.cs
@@ -8,7 +8,6 @@ namespace dev_learning.Constants
     public class ClaimsNames
     {
         public const string ID = "user_id";
-        public const string FullName = "full_name";
         public const string Email = "user_email";
         public const string Role = "user_role";
     }
