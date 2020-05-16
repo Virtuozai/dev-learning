7ca3adf5e2007b2ec39540f231de0ae9b118130b
hjuknonis <h.juknonis@gmail.com>
2020-05-16 15:33:23 +0300

    added isLearned

--- dev-learning/Models/User.cs
+++ dev-learning/Models/User.cs
@@ -14,8 +14,8 @@ namespace dev_learning.Models
     {
         public int Id { get; set; }
         public int? TeamId { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public UserRole Role { get; set; }
--- dev-learning/Models/UserSubject.cs
+++ dev-learning/Models/UserSubject.cs
@@ -10,6 +10,7 @@ namespace dev_learning.Models
         public User User { get; set; }
         public int SubjectId { get; set; }
         public Subject Subject { get; set; }
+        public bool IsLearned { get; set; }
         public DateTime StartDateTime { get; set; }
         public DateTime EndDateTime { get; set; }
     }
